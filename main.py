c2c18d52250368df97d3186cf1b41296c5f40ebd
brandon <brandonskerritt51@gmail.com>
2021-01-28 17:53:54 +0000

    added b64

--- main.py
+++ main.py
@@ -3,24 +3,26 @@ from fastapi.responses import JSONResponse
 import json
 from mangum import Mangum
 from name_that_hash import runner
+import base64
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-        return {"Hello": "World"}
+    return {"Hello": "World"}
 
 
 @app.get("/api/{hash_string}")
 def return_hash_id(hash_string: str):
     headers = {
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Origin': 'http://localhost:3000/',
-            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-        }
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "https://nth.skerritt.blog",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+    }
+    hash_string = base64.b64decoode(hash_string)
     output = json.loads(runner.api_return_hashes_as_json([hash_string]))
-    print(output)
     return JSONResponse(content=output[hash_string], headers=headers)
+
 
 handler = Mangum(app)
